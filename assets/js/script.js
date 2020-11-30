a5226895d7afa73aff53ea37d25cc89d29439ee8
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-30 20:09:38 +0100

    Finish all the task, still need to refactor and improve the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,39 +15,78 @@ $(document).ready(function () {
 // Codice viene eseguito solo quando documento e' carico
 $(function () {
   //codice jQuery
-  
-  
+
   // Variabili con valore
 
-  var features = $('.features');
-  var plans = $('.plans');
-  var aboutUs = $('.about_us');
-  var help = $(".help");
+  var features = $(".features");
+  var features_menu = $(".features_menu .card_hover");
+
+  var plans = $(".plans, .plans_menu > div ");
+  var plans_menu = $(".plans_menu > div");
+
+  var aboutUs = $(".about_us, .about_us_menu > .card_hover");
+  var aboutUs_menu = $(".about_us_menu > div");
+
+  var help = $(".help , .help_menu > div");
+  var help_menu = $(".help_menu > div");
 
   // Variabili senza valore
-  
-
-
-
 
   // Functions
 
-  $('.plans').hover(
+  //FEATURES Menu
+  $(".card_hover_features, .features").hover(
+    //this function will work on handlerIn
     function () {
-     
-     $(".plans_menu > div").removeClass('none');
-     $('.plans_menu > div').addClass('active');
+      features_menu.removeClass("none");
+      features_menu.addClass("active");
     },
-
+    //this function will work on handlerOut
     function () {
-       $(".plans_menu > div").removeClass("active");
-       $(".plans_menu > div").addClass("none");
-      }
-    
-    )
+      features_menu.removeClass("active");
+      features_menu.addClass("none");
+    }
+  );
 
+  //PLANS Menu
+  plans.hover(
+    //this function will work on handlerIn
+    function () {
+      plans_menu.removeClass("none");
+      plans_menu.addClass("active");
+    },
+    //this function will work on handlerOut
+    function () {
+      plans_menu.removeClass("active");
+      plans_menu.addClass("none");
+    }
+  );
 
+  //ABOUT US Menu
+  aboutUs.hover(
+    //this function will work on handlerIn
+    function () {
+      aboutUs_menu.removeClass("none");
+      aboutUs_menu.addClass("active");
+    },
+    //this function will work on handlerOut
+    function () {
+      aboutUs_menu.removeClass("active");
+      aboutUs_menu.addClass("none");
+    }
+  );
 
-
-
+  //HELP Menu
+  help.hover(
+    //this function will work on handlerIn
+    function () {
+      help_menu.removeClass("none");
+      help_menu.addClass("active");
+    },
+    //this function will work on handlerOut
+    function () {
+      help_menu.removeClass("active");
+      help_menu.addClass("none");
+    }
+  );
 });
